3f39b47cc018cd25a1bb4ab57f84232b5dee6425
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2021-03-23 17:20:25 -0500

    [.] NPM/WP: Inicializa Proyecto & Define comentario de cabecera del plugin

---
+++ lapizzeria-gutenberg.php
@@ -0,0 +1,13 @@
+/**
+ * Plugin Name:       La Pizzeria Gutenberg Blocks
+ * Plugin URI:        
+ * Description:       Bloques nativos de Gutenberg para La Pizzería
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Juan Carlos Jiménez Gutiérrez
+ * Author URI:        https://github.com/jcarlosj
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       plugin-lapizzeria-bkl
+*/
