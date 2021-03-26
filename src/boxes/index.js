38e1e1f6e897f68dc426ca9528034057968f5010
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2021-03-26 17:37:12 -0500

    [+] Plugin: Crea atributo para usar paleta de colores para agregar color de fondo al Bloque Boxes

--- src/boxes/index.js
+++ src/boxes/index.js
@@ -1,7 +1,8 @@
 const 
     { registerBlockType } = wp.blocks,
     { __ } = wp.i18n,
-    { useBlockProps, RichText } = wp.blockEditor;       /** Paso 1: Importar el/los componentes que se utilizarán. */
+    { useBlockProps, InspectorControls, ColorPalette, RichText } = wp.blockEditor,       /** Paso 1: Importar el/los componentes que se utilizarán. */
+    { PanelBody, PanelRow } = wp.components;
 
 /** Logo */
 import { ReactComponent as Logo } from '../logo.svg';
@@ -25,6 +26,9 @@ registerBlockType( 'lapizzeria/block-boxes', {
             type: 'string',
             source: 'html',
             selector: '.box p'
+        },
+        bgColor : {
+            type: 'string'
         }
     },
     edit: ( props ) => {    /** Paso 4: Se obtienen los props destructurados */
@@ -32,7 +36,7 @@ registerBlockType( 'lapizzeria/block-boxes', {
         
         const 
             blockProps = useBlockProps( { className: 'box' } ),
-            { attributes: { title, text }, setAttributes } = props;    /** Paso 5: Extraer el contenido de los props (Destructurando) */
+            { attributes: { title, text, bgColor }, setAttributes } = props;    /** Paso 5: Extraer el contenido de los props (Destructurando) */
 
         /** Paso 3: Crear la función que lea los contenidos. */
         const onChangeTitle = ( newTitle ) => {
@@ -43,26 +47,44 @@ registerBlockType( 'lapizzeria/block-boxes', {
             console .log( text );
             setAttributes( { text } );    /** Paso 6: Guardar el contenido con setAttributes() */
         }
+        const onChangeBackgroundColor = ( bgColor ) => {
+            console .log( bgColor );
+            setAttributes( { bgColor });
+        }
 
         return (
-            <div { ...blockProps }>
-                <h2>
-                    <RichText 
-                        value={ title }         /** Hará que el campo siempre tenga el valor actual del campo */
-                        placeholder={ __( 'Add the title', 'plugin-lapizzeria-bkl' ) }  /** Paso 2: Colocar el/los componentes donde se desean usar. (Mostrar este texto antes de que el usuario haya agregado contenido) */  
-                        onChange={ onChangeTitle }                                       /** Paso 3: Enlaza función que lee los contenidos del campo RichText. */
-                        // onChange={ ( content ) => setAttributes( { content } ) }      /** Abrevia pasos 3 y 4 */
-                    />
-                </h2>
-                <p>
-                    <RichText 
-                        value={ text }         /** Hará que el campo siempre tenga el valor actual del campo */
-                        placeholder={ __( 'Add the text', 'plugin-lapizzeria-bkl' ) }  /** Paso 2: Colocar el/los componentes donde se desean usar. (Mostrar este texto antes de que el usuario haya agregado contenido) */  
-                        onChange={ onChangeText }                                       /** Paso 3: Enlaza función que lee los contenidos del campo RichText. */
-                        // onChange={ ( content ) => setAttributes( { content } ) }      /** Abrevia pasos 3 y 4 */
-                    />
-                </p>
-            </div>
+            <>
+                <InspectorControls>
+                    <PanelBody
+                        title={ __( 'Background color', 'plugin-lapizzeria-bkl' ) }
+                        initialOpen={ true }    /** Solo se permite que un panel abra automaticamente */
+                    >
+                        <PanelRow>{ __( 'Select background color', 'plugin-lapizzeria-bkl' ) }</PanelRow>
+                        <ColorPalette 
+                            onChange={ onChangeBackgroundColor }
+                            value={ bgColor }
+                        />
+                    </PanelBody>
+                </InspectorControls>
+                <div { ...blockProps } style={{ backgroundColor: bgColor }}>
+                    <h2>
+                        <RichText 
+                            value={ title }         /** Hará que el campo siempre tenga el valor actual del campo */
+                            placeholder={ __( 'Add the title', 'plugin-lapizzeria-bkl' ) }  /** Paso 2: Colocar el/los componentes donde se desean usar. (Mostrar este texto antes de que el usuario haya agregado contenido) */  
+                            onChange={ onChangeTitle }                                       /** Paso 3: Enlaza función que lee los contenidos del campo RichText. */
+                            // onChange={ ( content ) => setAttributes( { content } ) }      /** Abrevia pasos 3 y 4 */
+                        />
+                    </h2>
+                    <p>
+                        <RichText 
+                            value={ text }         /** Hará que el campo siempre tenga el valor actual del campo */
+                            placeholder={ __( 'Add the text', 'plugin-lapizzeria-bkl' ) }  /** Paso 2: Colocar el/los componentes donde se desean usar. (Mostrar este texto antes de que el usuario haya agregado contenido) */  
+                            onChange={ onChangeText }                                       /** Paso 3: Enlaza función que lee los contenidos del campo RichText. */
+                            // onChange={ ( content ) => setAttributes( { content } ) }      /** Abrevia pasos 3 y 4 */
+                        />
+                    </p>
+                </div>
+            </>
         )
     },
     save: ( props ) => {    /** Paso 7: Leer los contenidos que deseamos guardar usando save(). (Muestra lo que a guardado la BD) */
@@ -70,10 +92,10 @@ registerBlockType( 'lapizzeria/block-boxes', {
         
         const 
             blockProps = useBlockProps .save(), 
-            { attributes: { title, text } } = props;    /** Paso 5: Extraer el contenido de los props (Destructurando) */
+            { attributes: { title, text, bgColor } } = props;    /** Paso 5: Extraer el contenido de los props (Destructurando) */
     
         return (
-            <div { ...blockProps }>
+            <div { ...blockProps } style={{ backgroundColor: bgColor }}>
                 <h2>
                     <RichText .Content
                         value={ title }
